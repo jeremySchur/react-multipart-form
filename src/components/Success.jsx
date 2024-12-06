5c659b63158140adfadcf1c046b8fbcdc4cf62a5
Jeremy Schur <jeremy.schur02@gmail.com>
2024-12-05 22:16:44 -0700

    update to clean up code

---
+++ src/components/Success.jsx
@@ -0,0 +1,12 @@
+
+const Success = () => {
+    return (
+        <div className="flex flex-col items-center m-auto">
+            <h1 className="text-xl font-bold">Success!</h1>
+            <hr className="w-full my-3" />
+            <h2 className="text-sm text-gray-700">Your form has been successfully submitted.</h2>
+        </div>
+    );
+};
+
+export default Success;
